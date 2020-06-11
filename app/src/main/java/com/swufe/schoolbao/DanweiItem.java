560b90c999965f2df29c5928f810f5ce7b74640b
zhycurry <961437386@qq.com>
2020-06-11 15:22:41 +0800

    实习岗位获取 ver1.0

---
+++ app/src/main/java/com/swufe/schoolbao/DanweiItem.java
@@ -0,0 +1,7 @@
+package com.swufe.schoolbao;
+
+public class DanweiItem {
+    private int id;
+    private String date;
+    private String danweiming;
+}
---
+++ app/src/main/java/com/swufe/schoolbao/MemoActivity.java
@@ -0,0 +1,14 @@
+package com.swufe.schoolbao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo);
+    }
+}
